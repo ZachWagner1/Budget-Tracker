6c0e8386d7a7f184392610291093616091f3f7bc
ZachWagner1 <zwag14@yahoo.com>
2020-09-26 21:14:47 -0500

    fixes

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -1,45 +1,55 @@
 const FILES_TO_CACHE = [
-    "../../index.html",
-    "./index.js",
-    "./idb.js",
-    "../css/styles.css"
-  ]
-  
-  const APP_PREFIX = 'BudgetTracker-';
-  const VERSION = 'version_01';
-  const CACHE_NAME = APP_PREFIX + VERSION;
-  
-  self.addEventListener('install', function(e) {
-    e.waitUntil(
-      caches.open(CACHE_NAME).then(function(cache) {
-        return cache.addAll(FILES_TO_CACHE);
+  "./index.html",
+  "./js/index.js",
+  "./js/idb.js",
+  "./css/styles.css",
+  "./manifest.json"
+]
+
+const APP_PREFIX = 'BudgetTracker-';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('installing cache: ' + CACHE_NAME)
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  )
+})
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      let cacheKeeplist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX);
       })
-    )
-  })
-  
-  self.addEventListener('activate', function(e) {
-    e.waitUntil(
-      caches.keys().then(function(keyList) {
-        let cacheKeeplist = keyList.filter(function(key) {
-          return key.indexOf(APP_PREFIX);
-        }) 
-        // store current cache
-        cacheKeeplist.push(CACHE_NAME);
-  
-        // delete old cache
-        return Promise.all(keyList.map(function(key, i) {
-          if (cacheKeeplist.indexOf(key) === -1) {
-            return caches.delete(keyList[i]);
-          }
-        }))
-      })
-    )
-  })
-  
-  self.addEventListener('fetch', function(e) {
-    e.respondWith(
-      caches.match(e.request).then(function(request) {
-        return request || fetch(e.request)
-      })
-    )
-  }) 
+      // store current cache
+      cacheKeeplist.push(CACHE_NAME);
+
+      // delete old cache
+      return Promise.all(keyList.map(function (key, i) {
+        if (cacheKeeplist.indexOf(key) === -1) {
+          console.log('deleting cache: ' + keyList[i]);
+          return caches.delete(keyList[i]);
+        }
+      }))
+    })
+  )
+})
+
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request : ' + e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) {
+        console.log('responding with cache : ' + e.request.url)
+        return request
+      } else {
+        console.log('file is not cached, fetching : ' + e.request.url)
+        return fetch(e.request)
+      }
+    })
+  )
+}) 
